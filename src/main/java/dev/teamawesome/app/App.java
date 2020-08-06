2574ee760a3f4df6ef18e5022b27d7aaf92a30a8
Amanda Cosentino <48041627+mandeezie@users.noreply.github.com>
2020-08-06 13:46:51 -0700

    Update App.java

--- src/main/java/dev/teamawesome/app/App.java
+++ src/main/java/dev/teamawesome/app/App.java
@@ -1,5 +1,6 @@
 package dev.teamawesome.app;
 
+import dev.teamawesome.controllers.DoctorController;
 import io.javalin.Javalin;
 
 public class App
@@ -9,6 +10,14 @@ public class App
         Javalin app = Javalin.create(
                 config->{config.enableCorsForAllOrigins();}
         ).start(7000);
+        
+        app.get("/doctors", DoctorController.getAllDoctors);
+        
+        app.get("/doctors/:id", DoctorController.getDoctorById);
+        
+        app.post("/doctors", DoctorController.createDoctor);
+        
+        app.put("/doctors", DoctorController.updateDoctor);
     }
 
 }
