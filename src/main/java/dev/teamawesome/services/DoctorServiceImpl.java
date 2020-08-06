f9454d41c432427046975cbd8813a22b455d2298
Rajiv Tatineni <rtatineni2@gmail.com>
2020-08-06 15:17:18 -0500

    added services fixed daoimpl interface

---
+++ src/main/java/dev/teamawesome/services/DoctorServiceImpl.java
@@ -0,0 +1,38 @@
+package dev.teamawesome.services;
+
+import java.util.List;
+
+import dev.teamawesome.daos.DoctorDAO;
+import dev.teamawesome.daos.DoctorDaoImpl;
+import dev.teamawesome.entities.Doctor;
+
+public class DoctorServiceImpl implements DoctorService {
+	
+	public DoctorDAO ddao = new DoctorDaoImpl();
+	public Doctor createDoctor(Doctor doctor) {
+		return this.ddao.createDoctor(doctor);
+	
+	}
+
+	public List<Doctor> getAllDoctors() {
+		return this.ddao.getAllDoctors();
+	
+	}
+	
+	public Doctor updateDoctor(Doctor doctor) {
+		return this.ddao.updateDoctor(doctor);
+	
+	}
+	
+	public Doctor getDoctorsById(int id) {
+		return this.ddao.getDoctorById(id);
+
+	}
+
+	@Override
+	public Doctor getDoctorById(int id) {
+		// TODO Auto-generated method stub
+		return this.ddao.getDoctorById(id);
+	
+	}
+}
